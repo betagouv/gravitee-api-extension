b3b5f8938d38c21c7601de92f10fee3b45cb0859
Stanislas Bernard <bernardstanislas@gmail.com>
2021-01-18 17:22:10 +0100

    :white_check_mark: Test application listing

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -24,13 +24,29 @@ describe("Server", () => {
   });
 
   it("lists the application ids", async (done) => {
-    const response = await app.inject({
+    const emptyResponse = await app.inject({
       method: "GET",
       url: "/applications",
     });
 
-    expect(response.statusCode).toBe(200);
-    expect(JSON.parse(response.body)).toEqual([]);
+    expect(emptyResponse.statusCode).toBe(200);
+    expect(JSON.parse(emptyResponse.body)).toEqual([]);
+
+    await app.db.applications.insert({
+      id: "yolo",
+    });
+
+    const listResponse = await app.inject({
+      method: "GET",
+      url: "/applications",
+    });
+
+    expect(listResponse.statusCode).toBe(200);
+    expect(JSON.parse(listResponse.body)).toEqual([
+      {
+        id: "yolo",
+      },
+    ]);
     done();
   });
 });
