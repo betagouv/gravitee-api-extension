01b8d679bddae3bd8ebc38bbd27e2c075c4ea25b
Stanislas Bernard <bernardstanislas@gmail.com>
2021-01-19 17:37:45 +0100

    :green_heart: Fix end-to-end tests

--- src/gravitee/gravitee.module.ts
+++ src/gravitee/gravitee.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Application } from 'src/gravitee/application.entity';
 import { GraviteeController } from 'src/gravitee/gravitee.controller';
+import { GraviteeService } from 'src/gravitee/gravitee.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Application])],
   controllers: [GraviteeController],
+  providers: [GraviteeService],
 })
 export class GraviteeModule {}
