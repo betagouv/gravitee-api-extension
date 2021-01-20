dedab5c45c0496a051e44e2cdf2c38dcceeee5e3
Stanislas Bernard <bernardstanislas@gmail.com>
2021-01-20 17:24:06 +0100

    :bug: :wrench: Fix typeorm config

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,7 +4,7 @@ export default registerAs('database', () => ({
   type: process.env.DATABASE_TYPE || 'postgres',
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT || 5432,
-  user: process.env.DATABASE_USER,
+  username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   autoLoadEntities: true,
